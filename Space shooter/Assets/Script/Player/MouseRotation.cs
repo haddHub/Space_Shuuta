ecd21eba7b67c31944baed143846de0b11aa7ab8
adrien.denis <adrien.denis@uclouvain.be>
2017-10-02 16:20:39 +0200

    First commit
    - Player movement protoype
    - Space assets folder

---
+++ Space shooter/Assets/Script/Player/MouseRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MouseRotation : MonoBehaviour 
+{
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+        direction.Normalize();
+
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, -angle);
+    }
+}
---
+++ Space shooter/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour 
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float[] movementInputs = ReadMovementInputs();
+        Vector3 movement = new Vector3(movementInputs[0], movementInputs[1], 0f);
+
+        rb.velocity = movement * moveSpeed * Time.deltaTime;
+    }
+
+    private float[] ReadMovementInputs()
+    {
+        return new float[] { Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") };
+    }
+}
