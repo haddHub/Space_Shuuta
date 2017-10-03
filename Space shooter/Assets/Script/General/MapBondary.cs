6bd5ae2e90601b81f345621be46ee01c22e6e416
adrien.denis <adrien.denis@uclouvain.be>
2017-10-03 11:12:09 +0200

    Map bondary
    - Bondary around the map
    - Player can be limited by the map bondary

---
+++ Space shooter/Assets/Script/General/MapBondary.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MapBondary : MonoBehaviour 
+{
+    private BoxCollider2D col;  // The box collider that define the map limits
+
+    public static MapBondary instance; // Singleton
+    public Bondary bondary; // World postion of the bondary
+
+    private void Awake()
+    {
+        instance = this;
+        col = GetComponent<BoxCollider2D>();
+        bondary = new Bondary(col.bounds.min.x, col.bounds.max.x, col.bounds.min.y, col.bounds.max.y);
+    }
+
+    // Destroy everything that exit the map bondary
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name + "Destroyed !");
+        Destroy(collision.gameObject);
+    }
+}
+
+[System.Serializable]
+public class Bondary
+{
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+
+    public Bondary(float xmin, float xmax, float ymin, float ymax)
+    {
+        xMin = xmin;
+        xMax = xmax;
+        yMin = ymin;
+        yMax = ymax;
+    }
+}
--- Space shooter/Assets/Script/Player/PlayerMovement.cs
+++ Space shooter/Assets/Script/Player/PlayerMovement.cs
@@ -1,28 +1,51 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerMovement : MonoBehaviour 
 {
     [SerializeField]
-    private float moveSpeed;
+    private float moveSpeed;    // move speed
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb;     // my rigidbody2d
+    private Bondary mapBondary; // Limits of the map
+    private Vector2 sizeOffset; // Offset caused by the size of the sprite
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        mapBondary = MapBondary.instance?.bondary;
+        sizeOffset = new Vector2(GetComponent<Collider2D>().bounds.size.x / 2f, GetComponent<Collider2D>().bounds.size.y / 2f);
+    }
+
     private void FixedUpdate()
     {
+        // Read the current input
         float[] movementInputs = ReadMovementInputs();
         Vector3 movement = new Vector3(movementInputs[0], movementInputs[1], 0f);
-
+        // Apply inputs to the velocity
         rb.velocity = movement * moveSpeed * Time.deltaTime;
+
+        // Limit the player position to not exit the map limits
+        if (mapBondary != null)
+        {
+            transform.position = MapLimitedPositions();
+        }
     }
 
     private float[] ReadMovementInputs()
     {
         return new float[] { Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") };
+    }
+
+    private Vector2 MapLimitedPositions()
+    {
+        return new Vector2
+               (Mathf.Clamp(rb.position.x, mapBondary.xMin + sizeOffset.x, mapBondary.xMax - sizeOffset.x),
+               Mathf.Clamp(rb.position.y, mapBondary.yMin + sizeOffset.y, mapBondary.yMax - sizeOffset.y));
     }
 }
